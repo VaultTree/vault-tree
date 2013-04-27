6529535b6cd24ffd4e43b36c69f25b2a48fb4d43
Andrew Bashelor <abash1212@gmail.com>
2013-04-26 22:39:16 -0400

    Refactor Vault Spec

--- spec/lock_smith/vault_spec.rb
+++ spec/lock_smith/vault_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 module LockSmith
-  describe 'SymmetricVault' do
+  describe 'Vault' do
     before :all do
-      @json = Fixtures.symmetric_vault
+      #@locked_vault = Fixtures.locked_vault
+      #@unlocked_vault = Fixtures.unlocked_vault
     end
 
     describe '#vault_rack' do
       it 'returns the correct vault rack object' do
-        vr = SymmetricVault.new(@json).vault_rack
+        pending
+        vr = Vault.new(@json).vault_rack
         vr.as_json.should == Fixtures.vault_rack
       end
     end
@@ -22,18 +24,5 @@ module LockSmith
         pending
       end
     end
-
-    describe '#type' do
-      it 'returns symmetric vault' do
-        pending
-      end
-    end
-
-    describe '#lock' do
-    end
-
-    describe '#unlock' do
-    end
-
   end
 end

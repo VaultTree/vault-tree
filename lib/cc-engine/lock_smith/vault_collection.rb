7a40183ad984cadcb10e4ab362f4b19ec46bb780
Andrew Bashelor <abash1212@gmail.com>
2013-04-26 22:11:11 -0400

    Refactor vault collection spec into separate files

---
+++ lib/cc-engine/lock_smith/vault_collection.rb
@@ -0,0 +1,27 @@
+module LockSmith
+  class VaultCollection
+    attr_reader :string
+
+    def initialize(json = nil)
+      @string = json || empty
+    end
+
+    def [](id)
+      vault(id) || NullVault.new
+    end
+
+    private
+
+    def vault(id)
+      Vault.new(collection[id]) if collection[id]
+    end
+
+    def collection
+      MultiJson.load(string)
+    end
+
+    def empty
+      "[]"
+    end
+  end
+end
--- spec/lock_smith/vault_collection_spec.rb
+++ spec/lock_smith/vault_collection_spec.rb
@@ -21,36 +21,3 @@ module LockSmith
     end
   end
 end
-
-module LockSmith
-  class NullVault
-  end
-end
-
-module LockSmith
-  class VaultCollection
-    attr_reader :string
-
-    def initialize(json = nil)
-      @string = json || empty
-    end
-
-    def [](id)
-      vault(id) || NullVault.new
-    end
-
-    private
-
-    def vault(id) 
-      Vault.new(collection[id]) if collection[id]
-    end
-
-    def collection
-      MultiJson.load(string)
-    end
-
-    def empty
-      "[]"
-    end
-  end
-end

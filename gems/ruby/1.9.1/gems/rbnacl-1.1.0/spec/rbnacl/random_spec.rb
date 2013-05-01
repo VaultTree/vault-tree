09c97d8593cb139a28ab556c963b432b28c74099
Andrew Bashelor <abash1212@gmail.com>
2013-05-01 09:20:27 -0700

    Delete local Gems Directory

--- gems/ruby/1.9.1/gems/rbnacl-1.1.0/spec/rbnacl/random_spec.rb
+++
@@ -1,9 +0,0 @@
-# encoding: binary
-describe Crypto::Random do
-  it "produces random bytes" do
-    Crypto::Random.random_bytes(16).bytesize.should == 16
-  end
-  it "produces different random bytes" do
-    Crypto::Random.random_bytes(16).should_not == Crypto::Random.random_bytes(16)
-  end
-end

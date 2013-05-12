e900b4be450644440992dd24fbf38defee2dabf7
Andrew Bashelor <abash1212@gmail.com>
2013-05-11 20:21:16 -0400

    Basic functioning factory girl

--- db/migrate/001_create_models.rb
+++ db/migrate/001_create_models.rb
@@ -1,21 +1,24 @@
 class CreateModels < ActiveRecord::Migration
   def change
-    create_table :pages do |t|
-      t.text :content
-      t.boolean :published, default: false
+
+    create_table :enforcers do |t|
     end
 
     create_table :contracts do |t|
     end
 
     create_table :contract_headers do |t|
+      t.integer :contract_id
+      t.text :content
     end
 
     create_table :genesis_vaults do |t|
+      t.integer :contract_id
+      t.text :content
     end
 
     create_table :symmetric_vaults do |t|
-      t.text :contents, default: nil
+      t.text :content, default: nil
       t.text :encrypted_contents, default: nil
       t.boolean :open, default: false
     end
@@ -23,5 +26,9 @@ class CreateModels < ActiveRecord::Migration
     create_table :asymmetric_vaults do |t|
     end
 
+    create_table :vaults do |t|
+      t.integer :contract_id
+      t.text :content
+    end
   end
 end
--- spec/models/lock_smith/contract_spec.rb
+++ spec/models/lock_smith/contract_spec.rb
@@ -1,15 +1,63 @@
 require 'spec_helper'
-describe 'Contract' do
-  describe 'action' do
 
-    it 'pends' do
-      puts 'contract:'
-      puts VaultTree::Contract.new
-      pending 'Not Implemented'
+module FactoryHelpers
+  def self.random_hex
+    (0...4).map{ ('a'..'z').to_a[rand(26)] }.join
+  end
+end
+
+FactoryGirl.define do
+
+  factory :vault, class: VaultTree::Vault do
+    content "ENCRYPTED_CONTENT-#{FactoryHelpers.random_hex}"
+  end
+
+  factory :genesis_vault, class: VaultTree::GenesisVault do
+    content "ENCRYPTED_CONTENT-random_hex"
+  end
+
+  factory :contract_header, class: VaultTree::ContractHeader do
+    content "HEADER_CONTENT"
+  end
+end
+
+FactoryGirl.define do
+  factory :contract_with_vaults, class: VaultTree::Contract do
+    #after(:create) do |contract|
+      #contract.genesis_vault << FactoryGirl.create(:genesis_vault)
+    #end
+
+    after(:create) do |contract|
+      contract.vaults << FactoryGirl.create(:vault)
+      contract.vaults << FactoryGirl.create(:vault)
+      contract.vaults << FactoryGirl.create(:vault)
+      contract.contract_header = FactoryGirl.create(:contract_header)
+      contract.genesis_vault = FactoryGirl.create(:genesis_vault)
+    end
+  end 
+end
+
+describe 'Contract' do
+  describe '#create' do
+
+    before(:each) do
+      @contract = FactoryGirl.create(:contract_with_vaults)
     end
 
-    it 'pends' do
-      pending 'Not Implemented'
+    it 'has_many vaults' do
+      @contract.vaults.length.should be > 1
+    end
+
+    it 'each vault is different' do
+      pending
+    end
+
+    it 'has_one contract_header' do
+      @contract.contract_header.should be_an_instance_of(VaultTree::ContractHeader)
+    end
+
+    it 'has_one genesis_vault' do
+      @contract.genesis_vault.should be_an_instance_of(VaultTree::GenesisVault)
     end
   end
 end

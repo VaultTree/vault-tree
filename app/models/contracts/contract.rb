ad1cc1c4c18176dd59328b1b931b665f1ee12b97
Andy Bashelor <abash1212@gmail.com>
2013-05-25 00:57:50 -0400

    properly failing test for in and out

--- app/models/contracts/contract.rb
+++ app/models/contracts/contract.rb
@@ -7,10 +7,50 @@ module VaultTree
       ContractSerializer.new(presenter).to_json
     end
 
+    def self.import(json)
+      ContractDeserializer.new().build_contract(json)
+    end
+
     private
 
     def presenter
       ContractPresenter.new(self)
     end
+  end
+end
+
+module VaultTree
+  class ContractDeserializer
+    attr_reader :json
+
+    def build_contract(json)
+      assemble_vaults
+      assemble_parties
+      assemble_signatures
+      assemble_header
+    end
+
+    private
+
+    def contract_hash
+      @contract_hash ||= ActiveSupport::JSON.decode(json)
+    end
+
+    def assemble_vaults
+      puts 'assemble_vaults'
+    end
+
+    def assemble_parties
+      puts 'assemble_parties'
+    end
+
+    def assemble_signatures
+      puts 'assemble_signatures'
+    end
+
+    def assemble_header
+      puts 'assemble_header'
+    end
+
   end
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,38 +1,62 @@
 require 'spec_helper'
 
-describe 'Contract' do
-  describe '#create' do
+module VaultTree
+  describe 'Contract' do
 
-    before(:each) do
-      @contract = FactoryGirl.create(:contract)
+    describe '.import | #as_json' do
+      before(:each) do
+        @json = File.open(PathHelpers.one_two_three_contract).read
+        @contract = Contract.import(@json)
+      end
+
+      it 'the json matches after input and output' do
+        @contract.as_json.should == @json
+      end
+
+      it 'the compressed string checksums should match' do
+        pending
+      end
     end
 
-    it 'has_many vaults' do
-      @contract.vaults.length.should be > 1
+    describe '.import' do
+      it 'returns a null contract for invalid json' do
+        pending
+      end
     end
 
-    it 'can access its vaults' do
-      @contract.vaults.first.should be_an_instance_of(VaultTree::Vault)
-    end
+    describe '#create' do
 
-    it 'each vault is different' do
-      f = @contract.vaults.first.label
-      l = @contract.vaults.last.label
-      f.should_not == l
-    end
+      before(:each) do
+        @contract = FactoryGirl.create(:contract)
+      end
 
-    it 'has_many parties' do
-      @contract.parties.length.should be > 1
-    end
+      it 'has_many vaults' do
+        @contract.vaults.length.should be > 1
+      end
 
-    it 'can access its parties' do
-      @contract.parties.first.should be_an_instance_of(VaultTree::Party)
-    end
+      it 'can access its vaults' do
+        @contract.vaults.first.should be_an_instance_of(Vault)
+      end
 
-    it 'each party is different' do
-      f = @contract.parties.first.party_number
-      l = @contract.parties.last.party_number
-      f.should_not == l
+      it 'each vault is different' do
+        f = @contract.vaults.first.label
+        l = @contract.vaults.last.label
+        f.should_not == l
+      end
+
+      it 'has_many parties' do
+        @contract.parties.length.should be > 1
+      end
+
+      it 'can access its parties' do
+        @contract.parties.first.should be_an_instance_of(Party)
+      end
+
+      it 'each party is different' do
+        f = @contract.parties.first.party_number
+        l = @contract.parties.last.party_number
+        f.should_not == l
+      end
     end
   end
 end

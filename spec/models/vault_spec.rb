d74173b258d0dc46d5bc4378bb2b78e032ee71e0
Andy Bashelor <abash1212@gmail.com>
2013-05-24 21:16:11 -0400

    Remove text for node spec

--- spec/models/vault_spec.rb
+++ spec/models/vault_spec.rb
@@ -15,18 +15,3 @@ describe 'Vault' do
     end
   end
 end
-
-require 'spec_helper'
-
-describe 'Node' do
-  describe '#create' do
-
-    before(:each) do
-      @node = FactoryGirl.create(:node)
-    end
-
-    it 'has_one vault' do
-      @node.vault.should be_an_instance_of(VaultTree::Vault)
-    end
-  end
-end

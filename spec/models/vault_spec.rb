4270a1e6ca8bcf03c9f320c9e5ffb832fface093
Andy Bashelor <abash1212@gmail.com>
2013-05-24 21:13:59 -0400

    Remove symmetric_key_spec, unlocking_condition_spec, node_spec, enforcer_spec

--- spec/models/vault_spec.rb
+++ spec/models/vault_spec.rb
@@ -15,3 +15,18 @@ describe 'Vault' do
     end
   end
 end
+
+require 'spec_helper'
+
+describe 'Node' do
+  describe '#create' do
+
+    before(:each) do
+      @node = FactoryGirl.create(:node)
+    end
+
+    it 'has_one vault' do
+      @node.vault.should be_an_instance_of(VaultTree::Vault)
+    end
+  end
+end

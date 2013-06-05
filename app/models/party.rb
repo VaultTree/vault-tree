a018b681993e16950a137baddbce539481c28def
Andy Bashelor <abash1212@gmail.com>
2013-06-04 21:51:15 -0400

    Add setters to party model.

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,18 @@
 module VaultTree
   class Party < ActiveRecord::Base
     belongs_to :contract
+
+    def set_label(label)
+      @label = label
+    end
+
+    def set_verification_key(vk)
+      @verification_key = vk
+    end
+
+    def set_public_encryption_key(pek)
+      @public_encryption_key = pek
+    end
+
   end
 end

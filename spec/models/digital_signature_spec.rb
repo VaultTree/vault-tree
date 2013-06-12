388ef0e287754dd7c38ad29fbdfb3f7b69d7bd21
Andy Bashelor <abash1212@gmail.com>
2013-06-11 20:07:13 -0400

    Pending Digital Signature Specs

--- spec/models/digital_signature_spec.rb
+++ spec/models/digital_signature_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe 'DigitalSignature' do
   describe '#generate' do
+    it 'PENDING' do
+      pending
+    end
   end
 
   describe '#verify' do
+    it 'PENDING' do
+      pending
+    end
   end
 end

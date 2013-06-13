aa5a236657d82fee9a63b37eddba74458865925b
Andy Bashelor <abash1212@gmail.com>
2013-06-12 21:42:53 -0400

    Basic Passing Verification Key Setter

---
+++ app/api/verification_key_setter.rb
@@ -0,0 +1,25 @@
+module VaultTree
+  module V1
+    class VerificationKeySetter
+      attr_reader :json, :party_label, :verification_key
+
+      def initialize(json, opts = {})
+        @json = json
+        @party_label = opts[:party_label]
+        @verification_key = opts[:verification_key]
+      end
+
+      def run
+        contract.set_verification_key(verification_key, party_label)
+        contract.save
+        contract.as_json
+      end
+
+      private
+
+      def contract
+        @contract ||= Contract.import(json)
+      end
+    end
+  end
+end

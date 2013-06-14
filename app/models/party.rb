2746dbb3151a5dbb84f328109da16fdfe5514037
Andy Bashelor <abash1212@gmail.com>
2013-06-13 23:30:32 -0400

    Remove crufy setter methods

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,17 +3,5 @@ module VaultTree
     belongs_to :contract
     scope :with_label, lambda { |label| where(:label => label) }
 
-    def set_label(label)
-      @label = label
-    end
-
-    def set_verification_key(vk)
-      @verification_key = vk
-    end
-
-    def set_public_encryption_key(pek)
-      @public_encryption_key = pek
-    end
-
   end
 end

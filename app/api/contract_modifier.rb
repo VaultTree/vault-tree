2128c52bbb8c36398192ae6421692504f11718fe
Andy Bashelor <abash1212@gmail.com>
2013-06-15 19:46:42 -0400

    Move init functionality up into modifer class

--- app/api/contract_modifier.rb
+++ app/api/contract_modifier.rb
@@ -1,7 +1,21 @@
 module VaultTree
   module V1
     class ContractModifier
-      def initialize
+      attr_reader :json
+
+      def initialize(json, opts = {})
+        @json = json
+        post_initialize(opts)
+      end
+
+      def post_initialize(opts = {})
+        nil
+      end
+
+      private
+
+      def contract
+        @contract ||= VaultTree::Contract.import(json)
       end
     end
   end

1ef55aec77f457e2d44a850d59a6d6ab90076833
Andy Bashelor <abash1212@gmail.com>
2013-06-15 20:17:00 -0400

    Test Setup for party attribute

--- app/api/party_attribute_setter.rb
+++ app/api/party_attribute_setter.rb
@@ -1,6 +1,15 @@
 module VaultTree
   module V1
     class PartyAttributeSetter < ContractModifier
+      attr_reader :party_label, :attribute 
+
+      def post_initialize(opts = {})
+        @party_label = opts[:party_label]
+        @attribute = opts[:attribute]
+      end
+
+      def run
+      end
     end
   end
 end
--- spec/api/party_attribute_setter_spec.rb
+++ spec/api/party_attribute_setter_spec.rb
@@ -4,6 +4,45 @@ module VaultTree
   module V1
     describe 'PartyAttributeSetter' do
       describe '#run' do
+        context 'valid input' do
+          before :each do
+            @contract = FactoryGirl.create(:contract_with_bob).as_json
+          end
+
+          context 'for verification_key' do
+
+            before :each do
+              @opts = {attribute: :verification_key, party_label: "BOB"}
+              @returned_contract = PartyAttributeSetter.new(@contract,@opts).run
+            end
+
+            it 'returns the contract as a json string' do
+              pending
+              @returned_contract.should be_an_instance_of(String)
+            end
+
+            it 'the attribute has been set with the proper value' do
+              pending
+              @attr_value.should == @expected_attr_value
+            end
+          end
+
+          context 'for label' do
+            it 'the attribute has been set with the proper value' do
+              pending
+              @attr_value.should == @expected_attr_value
+            end
+          end
+
+        end
+
+        context 'invalid input' do
+          context 'for non_existant_attribute' do
+            it 'it raises an unsuppored attr exception' do
+              pending
+            end
+          end
+        end
       end
     end
   end

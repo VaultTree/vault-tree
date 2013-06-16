7a0b577282029436f146b4bd3e927c12992cf466
Andy Bashelor <abash1212@gmail.com>
2013-06-15 22:15:30 -0400

    Basic attr setter functionallity

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,5 +3,9 @@ module VaultTree
     belongs_to :contract
     scope :with_label, lambda { |label| where(:label => label) }
 
+    def set_attribute(attribute,value)
+      write_attribute(attribute, value)
+      self.save!
+    end
   end
 end
--- spec/api/party_attribute_setter_spec.rb
+++ spec/api/party_attribute_setter_spec.rb
@@ -12,17 +12,18 @@ module VaultTree
           context 'for verification_key' do
 
             before :each do
-              @opts = {attribute: :verification_key, party_label: "BOB"}
-              @returned_contract = PartyAttributeSetter.new(@contract,@opts).run
+              @expected_attr_value = "TEST_PARTY_ATTR"
+              @opts = {attribute: :verification_key, value: @expected_attr_value, party_label: "BOB"}
+              @returned_contract = PartyAttributeSetter.new(@contract, @opts).run
+              @returned_contract_hash =  Support::JSON.decode(@returned_contract)
             end
 
             it 'returns the contract as a json string' do
-              pending
               @returned_contract.should be_an_instance_of(String)
             end
 
             it 'the attribute has been set with the proper value' do
-              pending
+              @attr_value = @returned_contract_hash["parties"].select{|p| p["label"] == "BOB"}.first["verification_key"]
               @attr_value.should == @expected_attr_value
             end
           end

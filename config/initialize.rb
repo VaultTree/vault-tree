2bb317a349ef6f904a812aa878a6238b675590cf
Andy Bashelor <abash1212@gmail.com>
2013-08-10 12:19:49 -0400

    Remove References to SQLite3 and Active Record

--- config/initialize.rb
+++ config/initialize.rb
@@ -4,10 +4,10 @@
 require_relative 'initializers/dependencies'
 require_relative 'initializers/path_helpers'
 require_relative 'initializers/version'
-require_relative 'initializers/database'
 require_relative 'initializers/app'
 require_relative 'initializers/lib'
 require_rel 'initializers/monkey_patches'
 
 # connect to the database
-VaultTree::DataBase.new.establish_connection
+# require_relative 'initializers/database'
+# VaultTree::DataBase.new.establish_connection

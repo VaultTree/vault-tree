8123ab8b48af51e3808a0673ec2cf9a9ed5d9bd7
Andy Bashelor <abash1212@gmail.com>
2013-10-06 14:14:03 -0400

    Clean up some dependencies

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,3 @@
-require 'simplecov'
-
-#un comment below to use Simple Cove
-#SimpleCov.start
-
 require_relative '../../config/initialize'
-require 'aruba/cucumber'
 require 'factory_girl_rails'
 require_all VaultTree::PathHelpers.factories

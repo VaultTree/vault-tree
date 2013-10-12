389a9dc4db4d29d9b895c3b039b7f5a40497be5d
Andy Bashelor <abash1212@gmail.com>
2013-10-11 20:25:40 -0400

    Shift require statements to vault-tree.rb

---
+++ lib/vault-tree.rb
@@ -0,0 +1,8 @@
+# require_relative - Ruby > 1.9 method
+# require_rel - 'gem require_all' helper method to require all relative
+
+require_relative '../config/initializers/dependencies'
+require_relative '../config/initializers/path_helpers'
+require_relative '../config/initializers/version'
+require_relative '../config/initializers/lib'
+require_rel '../config/initializers/monkey_patches/**/*.rb'

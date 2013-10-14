284cc074e9c45beab5c360abdb556acf67e5300e
Andy Bashelor <abash1212@gmail.com>
2013-10-14 19:35:13 -0400

    Functioning Gemspec from Bundler Scaffolding

--- lib/vault-tree/config/dependencies.rb
+++ lib/vault-tree/config/dependencies.rb
@@ -1,10 +1,8 @@
 # encoding: utf-8
 require 'active_support'
 require 'json'
-require 'rainbow'
 require 'digest/sha1'
 require 'yaml'
 require 'rbnacl'
-require 'rbnacl/encoders/base32'
 require 'require_all'
 require 'erb'

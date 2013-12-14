955cff0229080e6a12dc391d80d426decab50d21
Andy Bashelor <abash1212@gmail.com>
2013-12-14 14:12:24 -0500

    Testing scaffolding for mruby.

--- features/native/vault-tree-native.steps.rb
+++ features/native/vault-tree-native.steps.rb
@@ -1,14 +1,28 @@
 require 'aruba/cucumber'
 
 Given(/^a proper mruby setup$/) do
-  VaultTree::NativeSetup.new().proper_setup? ? true : pending('SKIPPED. MRUBY NOT SETUP PROPERLY.')
+  VaultTree::Native::Setup.new().proper_setup? ? true : pending('SKIPPED. MRUBY ENV NOT SETUP PROPERLY.')
 end
 
 module VaultTree
-  class NativeSetup
-    def proper_setup?
-      false
+  module Native
+    class Setup
+      def proper_setup?
+        #inside_vagrant_vm? && mruby_dir_present? && mruby_exec_present?
+        false
+      end
+
+      def inside_vagrant_vm?
+        PathHelpers.project_parent_dir == '/vagrant'
+      end
+
+      def mruby_dir_present?
+        File.exists?(PathHelpers.mruby_dir)
+      end
+
+      def mruby_exec_present?
+        File.exists?(PathHelpers.mruby_exec)
+      end
     end
   end
 end
-

25170465dae1a0867a77a2cc29ecdd22b9655c6e
Andy Bashelor <abash1212@gmail.com>
2014-05-14 11:06:15 -0400

    Introduce SHAMIR_SHARE Keyword

---
+++ lib/vault-tree/keywords/shamir_share.rb
@@ -0,0 +1,18 @@
+module VaultTree
+  class ShamirShare < Keyword
+    attr_reader :share_collection_vault, :collection_index
+
+    def post_initialize(arg_array)
+      @share_collection_vault = arg_array[0]
+      @collection_index = arg_array[1]
+    end
+
+    def evaluate
+      share_collection[collection_index]
+    end
+
+    def share_collection
+      JSON.parse contract.retrieve_contents(share_collection_vault)
+    end
+  end
+end

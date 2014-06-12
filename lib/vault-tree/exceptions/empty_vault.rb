a140a6c9d902276d14a4dbcd8df5f2c081d31bdc
Andy Bashelor <abash1212@gmail.com>
2014-06-12 19:43:52 -0400

    Remove some duplication in exception presentations.

--- lib/vault-tree/exceptions/empty_vault.rb
+++ lib/vault-tree/exceptions/empty_vault.rb
@@ -6,28 +6,13 @@ module VaultTree
         @vault_id = params[:vault_id]
       end
 
-      def name
-        'Empty Vault'
-      end
+      def runtime_information
+        %Q{The following vault was empty: #{@vault_id}
 
-      def search_word
-        'empty_vault'
-      end
-
-      def background
-        %Q{
           Vault Tree does not allow you to open an empty vault.
           An empty vault means the the vault contents field is an empty
           string.
-        }
-      end
 
-      def runtime_information
-        %Q{The following vault was empty: #{@vault_id}}
-      end
-
-      def troubleshooting_questions
-        %Q{
           * Are you closing the vaults in the right order?
           * Are you attempting to open a vault before all
             of the vaults that it references have been closed?
--- lib/vault-tree/exceptions/failed_lock_attempt.rb
+++ lib/vault-tree/exceptions/failed_lock_attempt.rb
@@ -7,14 +7,6 @@ module VaultTree
         @locking_key = params[:locking_key]
       end
 
-      def name
-        'Failed Lock Attempt'
-      end
-
-      def search_word
-        'failed_lock_exception'
-      end
-
       def runtime_information
         %Q{
           Attempted to Lock Vault:

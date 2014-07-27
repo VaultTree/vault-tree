7ed8d1589d0fb289c8012e47e0816f94167aed3f
Andy Bashelor <abash1212@gmail.com>
2014-07-26 20:10:22 -0400

    Return JSON from close_vault method
    * now open_vault and close_vault return as string
    * update tests to support new public interface

--- features/steps/core.steps.rb
+++ features/steps/core.steps.rb
@@ -1,58 +1,50 @@
-Given(/^I have a blank reference contract$/) do
-  contract_path = VaultTree::PathHelpers.reference_contract
-  @contract_json = File.read(contract_path)
-end
-
 When(/^I lock away a random vault key$/) do
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract = @contract.close_vault('random_vault_key')
+  @contract = VaultTree::Contract.new(@contract).as_json
+  @contract = VaultTree::Contract.new(@contract).close_vault('random_vault_key')
 end
 
 When(/^I use the random key to lock a message$/) do
-  @contract = VaultTree::Contract.new(@contract_json)
+  @contract = VaultTree::Contract.new(@contract).as_json
   @msg = "CONGRATS! YOU OPENED THE VAULT."
-  @contract = @contract.close_vault('message_locked_with_random', msg: @msg)
+  @contract = VaultTree::Contract.new(@contract).close_vault('message_locked_with_random', msg: @msg)
 end
 
 Then(/^I can recover the message with the Random Key$/) do
-  @contract.open_vault('message_locked_with_random').should == @msg
+  VaultTree::Contract.new(@contract).open_vault('message_locked_with_random').should == @msg
 end
 
 When(/^I put this random key in an unlocked vault$/) do
-  @contract = @contract.close_vault('unlocked_random_key')
+  @contract = VaultTree::Contract.new(@contract).close_vault('unlocked_random_key')
 end
 
 Then(/^another user can recover the message with the Unlocked Random Key$/) do
-  @contract = @contract.close_vault('message_locked_with_unlocked_random_number')
-  @contract_json = @contract.as_json
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract.open_vault('message_locked_with_unlocked_random_number').should == @msg
+  @contract = VaultTree::Contract.new(@contract).close_vault('message_locked_with_unlocked_random_number')
+  @contract = VaultTree::Contract.new(@contract).as_json
+  VaultTree::Contract.new(@contract).open_vault('message_locked_with_unlocked_random_number').should == @msg
 end
 
 Given(/^I have access to the another user's unlocked public key$/) do
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract = @contract.close_vault('another_decryption_key')
-  @contract = @contract.close_vault('another_public_key')
-  @contract_json = @contract.as_json
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract = @contract.close_vault('my_decryption_key')
-  @contract = @contract.close_vault('my_public_key')
+  @contract = VaultTree::Contract.new(@contract).as_json
+  @contract = VaultTree::Contract.new(@contract).close_vault('another_decryption_key')
+  @contract = VaultTree::Contract.new(@contract).close_vault('another_public_key')
+  @contract = VaultTree::Contract.new(@contract).as_json
+  @contract = VaultTree::Contract.new(@contract).as_json
+  @contract = VaultTree::Contract.new(@contract).close_vault('my_decryption_key')
+  @contract = VaultTree::Contract.new(@contract).close_vault('my_public_key')
 end
 
 Given(/^I lock a simple message with a DH Key$/) do
-  @contract_json = @contract.as_json
+  @contract = VaultTree::Contract.new(@contract).as_json
   @asymmetric_message = "CONGRATS! YOU OPENED THE ASYMMETRIC VAULT."
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract = @contract.close_vault('asymmetric_message', asymmetric_message: @asymmetric_message)
+  @contract = VaultTree::Contract.new(@contract).close_vault('asymmetric_message', asymmetric_message: @asymmetric_message)
 end
 
 When(/^I transfer the contract to the other user$/) do
-  @contract_json = @contract.as_json
-  @contract = VaultTree::Contract.new(@contract_json)
+  @contract = VaultTree::Contract.new(@contract).as_json
 end
 
 Then(/^they can create a DH Key and unlock the message$/) do
-  @contract.open_vault('asymmetric_message').should == @asymmetric_message
+  VaultTree::Contract.new(@contract).open_vault('asymmetric_message').should == @asymmetric_message
 end
 
 Given(/^Consent keys for parties A, B, and C$/) do
@@ -62,25 +54,24 @@ Given(/^Consent keys for parties A, B, and C$/) do
 end
 
 When(/^I lock a away the consent keys$/) do
-  @contract = @contract.close_vault('a_consent_key', a_secret: @a_secret)
-  @contract = @contract.close_vault('b_consent_key', b_secret: @b_secret)
-  @contract = @contract.close_vault('c_consent_key', c_secret: @c_secret)
-  @contract_json = @contract.as_json
+  @contract = VaultTree::Contract.new(@contract).close_vault('a_consent_key', a_secret: @a_secret)
+  @contract = VaultTree::Contract.new(@contract).close_vault('b_consent_key', b_secret: @b_secret)
+  @contract = VaultTree::Contract.new(@contract).close_vault('c_consent_key', c_secret: @c_secret)
+  @contract = VaultTree::Contract.new(@contract).as_json
 end
 
 When(/^I lock a message in a vault using a split key$/) do
   @abc_consent_message = "A, B, AND C ALL AGREED TO OPEN THE VAULT."
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract = @contract.close_vault('abc_joint_consent_key',
+  @contract = VaultTree::Contract.new(@contract).close_vault('abc_joint_consent_key',
                                      a_secret: @a_secret,
                                      b_secret: @b_secret,
                                      c_secret: @c_secret)
-  @contract = @contract.close_vault('abc_consent_message',
+  @contract = VaultTree::Contract.new(@contract).close_vault('abc_consent_message',
                                      consent_message: @abc_consent_message,
                                      a_secret: @a_secret,
                                      b_secret: @b_secret,
                                      c_secret: @c_secret)
-  @contract_json = @contract.as_json
+  @contract = VaultTree::Contract.new(@contract).as_json
 end
 
 Then(/^I can recover the message if each party gives consent$/) do
@@ -89,9 +80,9 @@ Then(/^I can recover the message if each party gives consent$/) do
     b_secret: "B_SECRET_CONSENT_KEY",
     c_secret: "C_SECRET_CONSENT_KEY"
   }
-  @contract = VaultTree::Contract.new(@contract_json)
-  @contract.open_vault('abc_consent_message', @unlocking_consent).should == @abc_consent_message
-  puts @contract.open_vault('abc_consent_message')
+  @contract = VaultTree::Contract.new(@contract).as_json
+  VaultTree::Contract.new(@contract).open_vault('abc_consent_message', @unlocking_consent).should == @abc_consent_message
+  puts VaultTree::Contract.new(@contract).open_vault('abc_consent_message', @unlocking_consent)
 end
 
 Then(/^I cannot recover the message if one party fails to give consent$/) do
@@ -100,21 +91,20 @@ Then(/^I cannot recover the message if one party fails to give consent$/) do
     b_secret: "B_SECRET_CONSENT_KEY",
     c_secret: "C_SECRET_CONSENT_KEY"
   }
-  @contract = VaultTree::Contract.new(@contract_json)
-  expect{@contract.open_vault('abc_consent_message',@incomplete_unlocking_consent_keys)}.to raise_error(VaultTree::Exceptions::FailedUnlockAttempt)
+  @contract = VaultTree::Contract.new(@contract).as_json
+  expect{VaultTree::Contract.new(@contract).open_vault('abc_consent_message',@incomplete_unlocking_consent_keys)}.to raise_error(VaultTree::Exceptions::FailedUnlockAttempt)
 end
 
 Given(/^the blank contract:$/) do |string|
-  @contract_json = string
-  @contract = VaultTree::Contract.new(@contract_json)
+  @contract = string
 end
 
 When(/^I lock a message in a vault using a symmetric vault key$/) do
-  @contract = VaultTree::Contract.new(@contract_json)
+  @contract = VaultTree::Contract.new(@contract).as_json
   @msg = "CONGRATS! YOU OPENED THE VAULT."
-  @contract = @contract.close_vault('message', msg: @msg)
+  @contract = VaultTree::Contract.new(@contract).close_vault('message', msg: @msg)
 end
 
 Then(/^I can recover the message using the same key$/) do
-  @contract.open_vault('message').should == @msg
+  VaultTree::Contract.new(@contract).open_vault('message').should == @msg
 end
--- features/steps/external_input.steps.rb
+++ features/steps/external_input.steps.rb
@@ -1,17 +1,17 @@
 When(/^I lock the external input in a vault using a symmetric vault key$/) do
   @secret = "#{VaultTree::LockSmith.new(message: 'secret_string').secure_hash}"
-  @contract = VaultTree::Contract.new(@contract_json)
+  @contract = VaultTree::Contract.new(@contract).as_json
   @external_input = {msg: 'You Opened the Vault!', secret: @secret}
-  @contract = @contract.close_vault('message', @external_input)
+  @contract = VaultTree::Contract.new(@contract).close_vault('message', @external_input)
 end
 
 Then(/^I can recover the input message using the same key$/) do
-  @contract.open_vault('message', secret: @secret).should == @external_input[:msg]
+  VaultTree::Contract.new(@contract).open_vault('message', secret: @secret).should == @external_input[:msg]
 end
 
 When(/^I lock the external input in a vault using an external key$/) do
   @secret = "MY SECRET KEY THAT IS NOT THE PROPER NUMBER OF BYTES"
-  @contract = VaultTree::Contract.new(@contract_json)
+  @contract = VaultTree::Contract.new(@contract).as_json
   @external_input = {msg: 'You Opened the Vault!', secret: @secret}
-  @contract = @contract.close_vault('message', @external_input)
+  @contract = VaultTree::Contract.new(@contract).close_vault('message', @external_input)
 end

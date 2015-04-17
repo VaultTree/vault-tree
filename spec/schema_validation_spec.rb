0a47d951e695922b75a2772ebde5b3db39389dbf
Andy Bashelor <abash1212@gmail.com>
2015-04-17 15:17:55 -0400

    Add a second schema validation example

--- spec/schema_validation_spec.rb
+++ spec/schema_validation_spec.rb
@@ -25,3 +25,27 @@ describe '#single_vault.json' do
     validation_result.must_equal(true)
   end
 end
+
+symetric_key_vault = %Q{
+  {
+    "vaults":{
+      "vault_id":{
+        "contents": {"vault_collection": ["id_one","id_two"]},
+        "lock_key": {
+          "asym_pub_key": {"computed_pub_key": {"vault_contents":"id"}},
+          "asym_prv_key": {"external_input":""}
+        },
+        "unlock_key": {
+          "sym_key": {"generated_key": "open_asym_prv"}
+        },
+        "ciphertext": "" }
+    }
+  }
+}
+
+describe 'symetric key vault' do
+  it 'validates against the schema' do
+    validation_result = JSON::Validator.validate('schemas/schema.json', symetric_key_vault, :validate_schema => true, strict: true )
+    validation_result.must_equal(true)
+  end
+end

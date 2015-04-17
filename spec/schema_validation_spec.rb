73a5ba2a3f7f9f3916044858513d419b1af9da3d
Andy Bashelor <abash1212@gmail.com>
2015-04-17 15:18:58 -0400

    Bring over malformed example contract. Failing Spec.

--- spec/schema_validation_spec.rb
+++ spec/schema_validation_spec.rb
@@ -28,17 +28,22 @@ end
 
 symetric_key_vault = %Q{
   {
-    "vaults":{
-      "vault_id":{
-        "contents": {"vault_collection": ["id_one","id_two"]},
-        "lock_key": {
-          "asym_pub_key": {"computed_pub_key": {"vault_contents":"id"}},
-          "asym_prv_key": {"external_input":""}
+    "header": {},
+    "vaults": {
+      "random_vault_key":{
+        "description":"Random Number",
+        "contents": "RANDOM_KEY",
+        "lock_key": "UNLOCKED",
+        "unlock_key": "UNLOCKED",
+        "ciphertext": ""
         },
-        "unlock_key": {
-          "sym_key": {"generated_key": "open_asym_prv"}
-        },
-        "ciphertext": "" }
+      "message":{
+        "description": "Simple Congratulations Message",
+        "contents": "EXTERNAL_INPUT['msg']",
+        "lock_key": "KEY['random_vault_key']",
+        "unlock_key": "KEY['random_vault_key']",
+        "ciphertext": ""
+      }
     }
   }
 }
